65fc2a5d2480f0271dea6d1f5b2d13acfad7b186
Arvind Shokal <arvindshokal20895@gmail.com>
2016-09-13 18:55:56 +0530

    Calculator app basic done

--- MaterialCalc/js/script.js
+++ MaterialCalc/js/script.js
@@ -1,5 +1,6 @@
 var res="";
 var equalRes;
+var ans=0;
 function changeResult(buttId){
 	var buttVal=buttId.slice(4);
 	/* console.log(buttVal); */
@@ -7,25 +8,37 @@ function changeResult(buttId){
 		{
 			if(buttVal=="ca"){
 				res="";
+				$("#input_val2").text(res);
+				$("#input_val1").text("Ans= "+ans);
 			}
 			else if(buttVal==="co")
 			{
 				res=res.slice(0,-1);
+				$("#input_val2").text(res);
+				$("#input_val1").text("Ans= "+ans);
 			}
 			else if(buttVal==="equal"){
-				try{equalRes=eval(res);
-					 }
-				catch(e){console.log(e.message);
-								equalRes="Syntax Error"}
-				console.log(equalRes);
-				$("#input_val").text(res+"=");
-				$("#input_val2").text(equalRes);
-				res=equalRes;
+                if(res.charAt(res.length-1)!="=")
+                {
+					try{equalRes=eval(res);
+						 }
+					catch(e){console.log(e.message);
+									equalRes="Syntax Error"}
+					console.log(equalRes);
+					$("#input_val1").text(res+"=");
+					$("#input_val2").text(equalRes);
+					ans=equalRes;
+					res="";
+					$("#input_val2").text(ans);
+				}
 			}
 		}
-	else
+	else{
 		res=res.concat(buttVal);
-	$("#input_val2").text(res);
+		$("#input_val2").text(res);
+		$("#input_val1").text("Ans= "+ans);
+	}
+	
 	console.log(res);
 }
 $(document).ready(function(){
