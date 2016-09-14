78b7dbc13a737f3244d4afb0875ab9a9427b46c1
Arvind Shokal <arvindshokal20895@gmail.com>
2016-09-14 11:54:23 +0530

    Wiki search with all functionality

--- Wikiviewr/js/wiki.js
+++ Wikiviewr/js/wiki.js
@@ -8,17 +8,16 @@ function wikiSearch(squery){
       console.log(json);
       var titleVal;
       var desc;
-      $(".search-list").remove();
-      $(".main-box").append('<ul class="collapsible search-list" data-collapsible="accordion"></ul>')
+  
+      
       for (var i = 0; i < 10; i++) {
         console.log(json.query.search[i].title);
         titleVal=json.query.search[i].title;
         desc=json.query.search[i].snippet;
-        $(".search-list").append('<li id="list'+i+'"></li>');
-        $("#list"+i).append('<div class="collapsible-header">'+titleVal+'</div>');
+        $(".search-list").append('<li id="list'+i+'"><a href="page.html#'+titleVal+'" target="_blank">'+titleVal+'</a></li>');
+       // var currentItem = $("#list"+i);
 
       }
-      pageWiki();
      },
      error:function(errorText,errorThrown){
       console.log(errorText);      
@@ -26,37 +25,44 @@ function wikiSearch(squery){
 	});
 
 }
-function pageWiki(){
+/*function pageWiki(id,searchTerm){
+  console.log("strings got"+id+searchTerm);
   $.ajax({
-    url:'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=India',
+    url:'https://en.wikipedia.org/w/api.php?action=parse&format=json&page='+searchTerm,
     type : 'GET',
      dataType : 'jsonp',
     success: function(json){
-      console.log(json.parse.text);
-    $("main-box").append(json.parse.text);
-    $("#list0").append('<div class="collapsible-body">'+json.parse.text+'</div>');
+   // $("main-box").append(json.parse.text['*']);
+    $("#"+id).append('<p>'+json.parse.text['*']+'</p>');
 },
     error:function(errorText){
       console.log(errorText);
     }
   });
-}
+}*/
 $(document).ready(function(){
+  $("#queryText").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#searchButton").click();
+    }
+});
 $("#searchButton").on("click",function(){
  /* if(squery)
     {*/
+      $("#main-con").removeClass("main-box");
       var squery = $("#queryText").val();
       if(squery){
-      
       console.log("hello"+squery);
       wikiSearch(squery);
     }
 });
-$(".search-list").click(function(event) {
+
+/*$(".search-list").on('click',function(event) {
     var clickedId = event.target.id;
-    console.log(clickedId);
-    pageWIki();
-    console.log("cs");
-});
+    console.log("clicked id is"+clickedId);
+    var selectedOp=$("#"+clickedId).text();
+    console.log(selectedOp);
+    localStorage.setPage=selectedOp;
+});*/
 
 }); 
